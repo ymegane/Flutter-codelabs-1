9b6e6ba6b39aef767504acca290eed4522c8f18c
ymegane <crybaby.sumire+github@gmail.com>
2019-03-13 23:28:38 +0900

    Stap4-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(FriendlychatApp());
 
-class MyApp extends StatelessWidget {
+class FriendlychatApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Friendlychat',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Friendlychat'),
-        ),
-      ),
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Friendlychat"),
     );
   }
 }
