3ed0ec4296fc2acd47ad384ab6e380dc474d8061
ymegane <crybaby.sumire+github@gmail.com>
2019-03-13 23:38:54 +0900

    Stap5-1

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,35 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState()  => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final _textController =TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Friendlychat"),
+      appBar: AppBar(title: Text("Friendlychat")),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(
+          hintText: "Send a message" ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
