0df2508f896f808ecd10011d690db3c8b232c64b
ymegane <crybaby.sumire+github@gmail.com>
2019-03-14 00:03:45 +0900

    Stap5-2

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,11 @@ class FriendlychatApp extends StatelessWidget {
 
 class ChatScreen extends StatefulWidget {
   @override
-  State createState()  => ChatScreenState();
+  State createState() => ChatScreenState();
 }
 
 class ChatScreenState extends State<ChatScreen> {
-  final _textController =TextEditingController();
+  final _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,15 +30,28 @@ class ChatScreenState extends State<ChatScreen> {
   }
 
   Widget _buildTextComposer() {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: InputDecoration.collapsed(
-          hintText: "Send a message" ),
-      ),
-    );
+    return IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
+        child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration:
+                        InputDecoration.collapsed(hintText: "Send a message"),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textController.text)),
+                )
+              ],
+            )));
   }
 
   void _handleSubmitted(String text) {
